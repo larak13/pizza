56f001eb50b74881e6f5bfe3bec960e2d2ee4568
Lara Kristinsdottir <larakristinsdottir@Laras-MacBook.lan>
2017-12-07 18:28:55 +0000

    Bjo til xcode project

---
+++ Pizza/Pizza/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pizza
+//
+//  Created by Lara Kristinsdottir on 07/12/2017.
+//  Copyright © 2017 Lara Kristinsdottir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
